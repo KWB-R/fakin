23b47640da789104ae9befe4b2b9637e412fc712
mrustl <michael.rustler@kompetenz-wasser.de>
2019-02-10 02:53:08 +0100

    Fix hugo version

--- _build.sh
+++ _build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
   
-Rscript -e "blogdown::install_hugo(version = '0.54', force = TRUE)"
+Rscript -e "blogdown::install_hugo(version = '0.54.0', force = TRUE)"
 # Copying needed as blogdown expects "config.toml" in root_dir
 Rscript -e "file.copy('config/_default/config.toml', 'config.toml', overwrite = TRUE)"
 Rscript -e "blogdown::build_site()"
