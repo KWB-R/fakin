dfb20945ada90b9712c90307d548a1ee1fbce83f
mrustl <michael.rustler@kompetenz-wasser.de>
2019-02-19 18:40:56 +0100

    fix Travis build (see https://github.com/mrustl/mrustl.github.io/commit/9cd64deeb120825a5d127dd1382f066c147c8f92)

--- _build.sh
+++ _build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
   
 Rscript -e "blogdown::install_hugo(version = '0.54.0', force = TRUE)"
+Rscript -e "blogdown::hugo_version()"
 # Copying needed as blogdown expects "config.toml" in root_dir
 Rscript -e "file.copy('config/_default/config.toml', 'config.toml', overwrite = TRUE)"
 Rscript -e "blogdown::build_site()"
