2cd44f34e113b7faf525bc2f697111f374ac5838
mrustl <michael.rustler@kompetenz-wasser.de>
2019-09-12 09:37:00 +0200

    Use latest hugo version https://github.com/gohugoio/hugo/releases/tag/v0.58.1

--- _build.sh
+++ _build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
   
-Rscript -e "blogdown::install_hugo(version = '0.54.0', force = TRUE)"
+Rscript -e "blogdown::install_hugo(version = '0.58.1', force = TRUE)"
 Rscript -e "blogdown::hugo_version()"
 # Copying needed as blogdown expects "config.toml" in root_dir
 Rscript -e "file.copy('config/_default/config.toml', 'config.toml', overwrite = TRUE)"
